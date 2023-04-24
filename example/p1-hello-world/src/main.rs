1addf40024cd4f5c4585633b7f28f3dc284d3a7b
Andrew Schmid <andrewschmid123@gmail.com>
2023-04-24 10:24:08 -0500

    Finished hello world

--- example/p1-hello-world/src/main.rs
+++ example/p1-hello-world/src/main.rs
@@ -52,6 +52,50 @@ fn main() {
     let v = List(vec![1, 2, 3]);
 
     println!("{}", v);
+
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *city);
+    }
+
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *color);
+    }
 }
 
 #[derive(Debug)]
@@ -107,5 +151,45 @@ impl Display for List {
         }
 
         return write!(f, "]");
+    }
+}
+
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lat >= 0.0 { 'E' } else { 'W' };
+
+        write!(
+            f,
+            "{}: {:.3}°{} {:.3}°{}",
+            self.name,
+            self.lat.abs(),
+            lat_c,
+            self.lon.abs(),
+            lon_c
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "RGB ({}, {}, {}) 0x{:0>2x}{:0>2x}{:0>2x}",
+            self.red, self.green, self.blue, self.red, self.green, self.blue,
+        )
     }
 }
