93b030bd4147c5d832df8fd98258dc57e301579c
Andrew Schmid <andrewschmid-dev@proton.me>
2023-11-07 11:36:52 -0600

    Closures / Functional programming

---
+++ book/chapter-13-functional/src/main.rs
@@ -0,0 +1,46 @@
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let num_red = self.shirts
+            .iter()
+            .filter(|&&s| s == ShirtColor::Red)
+            .count();
+
+        let num_blue = self.shirts.iter()
+            .filter(|&&s| s == ShirtColor::Blue)
+            .count();
+
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue]
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!("The user with preference {:?} gets {:?}", user_pref1, giveaway1);
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!("The user with preference {:?} gets {:?}", user_pref2, giveaway2);
+}
