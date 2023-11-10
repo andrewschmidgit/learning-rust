4754214e53d8a01aeb54cd71014d66a8bd151df8
Andrew Schmid <andrewschmid-dev@proton.me>
2023-11-10 14:13:59 -0600

    Went over lots of patterns and matches

---
+++ book/chapter-18-match-the-pattern/src/main.rs
@@ -0,0 +1,123 @@
+fn main() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    let v = ['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    let (x, y, z) = (1, 2, 3);
+    println!("x: {x}, y: {y}, z: {z}");
+
+    print_coordinates(&(3, 6));
+
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50!"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("Something else"),
+    }
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("Something else"),
+    }
+
+    let p = Point {
+        x: 1,
+        y: 2
+    };
+
+    let Point { x: a, y: b } = p;
+    let Point { x, y } = p;
+
+    assert_eq!(1, a);
+    assert_eq!(2, b);
+    assert_eq!(1, x);
+    assert_eq!(2, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => println!("On neither axis: ({x}, {y})"),
+    }
+
+    let message = Message::ChangeColor(0, 160, 255);
+
+    match message {
+        Message::Quit => println!("Quit"),
+        Message::Move { x, y } => println!("Moving to ({x}, {y})"),
+        Message::Write(text) => println!("Writing: {text}"),
+        Message::ChangeColor(r, g, b) => println!("Change color to ({r}, {g}, {b})"),
+    }
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({x}, {y})");
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
